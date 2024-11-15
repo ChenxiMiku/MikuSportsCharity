6f653ef68287a1237ca632ab0fe25f7c8b4603c4
AkiyaKiko <sinchialveno@protonmail.com>
2024-11-15 17:44:29 +0800

    change js

--- js/checklogin.js
+++ js/checklogin.js
@@ -1,7 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const avatarImg = document.getElementById('avatarImg');
     const dropdownMenu = document.getElementById('dropdownMenu');
-    const logoutBtn = document.getElementById('logoutBtn');
+    const logoutButtons = document.querySelectorAll('.logoutBtn'); // 查询所有带有 'logoutBtn' 类的元素
     // 检查是否已登录
     function getLoginStatusFromCookie() {
         const cookies = document.cookie.split('; ');
@@ -14,34 +14,36 @@ document.addEventListener('DOMContentLoaded', function() {
         return false;
     }
 
-    isLoggedIn = getLoginStatusFromCookie();
-
-
+    const isLoggedIn = getLoginStatusFromCookie();
 
     // 登出功能
-    logoutBtn.addEventListener('click', function(event) {
-        event.preventDefault();
-
-        document.cookie = "isLoggedIn=false; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/;";
-        localStorage.removeItem('previousPage');
-        window.location.reload(); // 刷新页面
+    logoutButtons.forEach(function (logoutBtn) {
+        console.log("asd");
+        logoutBtn.addEventListener('click', function (event) {
+            event.preventDefault();
+            console.log("Logout button clicked");
+            document.cookie = "isLoggedIn=false; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/;";
+            localStorage.removeItem('previousPage');
+            //window.location.reload(); // 刷新页面
+        });
     });
 
     // 页面加载时显示登录状态
     if (isLoggedIn) {
-        document.getElementById('userAvatar').classList.remove('d-none')
-        if(document.getElementById('loginBtn')){
+
+        document.getElementById('userAvatar').classList.remove('d-none');
+        if (document.getElementById('loginBtn')) {
             document.getElementById('loginBtn').style.display = 'none';
         }
-        //document.getElementById('registerBtn').style.display = 'none';
         document.getElementById('userAvatar').style.display = 'block';
     } else {
-        document.getElementById('userLogin').classList.remove('d-none')
-        if(document.getElementById('loginBtn')){
+        if (document.getElementById('userLogin')) {
+            document.getElementById('userLogin').classList.remove('d-none');
+        }
+
+        if (document.getElementById('loginBtn')) {
             document.getElementById('loginBtn').style.display = 'block';
         }
-        //document.getElementById('registerBtn').style.display = 'block';
         document.getElementById('userAvatar').style.display = 'none';
     }
-
 });
